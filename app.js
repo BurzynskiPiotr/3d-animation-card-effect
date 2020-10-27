a712b2737774e87daf543ee5f6a6965bb2ca5351
Piotr <51122302+BurzynskiPiotr@users.noreply.github.com>
2020-10-27 14:32:26 +0100

    commit card animation project

---
+++ app.js
@@ -0,0 +1,44 @@
+// Movement Animation
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+// Items
+
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+// Moving Animation Event
+
+container.addEventListener("mousemove", (e) => {
+  let yAxis = (window.innerWidth / 2 - e.pageX) / 20;
+  let xAxis = (window.innerHeight / 2 - e.pageY) / 20;
+  card.style.transform = `rotateY(${-yAxis}deg) rotateX(${xAxis}deg)`;
+});
+
+// Animate In
+
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(100px)";
+  sneaker.style.transform = "translateZ(100px) rotateZ(-25deg)";
+  description.style.transform = "translateZ(90px)";
+  sizes.style.transform = "translateZ(60px)";
+  purchase.style.transform = "translateZ(65px)";
+});
+
+// Animate Out
+
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(${0}deg) rotateX(${0}deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});
